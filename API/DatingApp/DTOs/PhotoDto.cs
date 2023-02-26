5ed09926f77d8393c3fb556cf38c71f119864efb
edvin.alicic2020@outlook.com <edvin.alicic2020@outlook.com>
2023-02-26 20:21:35 +0100

    Seedovana baza podataka, dodan automapper

---
+++ API/DatingApp/DTOs/PhotoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.DTOs
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMail { get; set; }
+    }
+}
---
+++ API/DatingApp/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using System.Linq;
+
+namespace DatingApp.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
